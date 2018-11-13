9bddf6233b542f9f82e42a8f8bd17f72c984b831
Hernan <hernandario35@gmail.com>
2018-11-13 14:57:14 -0500

    api

--- app/controllers/aliados_controller.rb
+++ app/controllers/aliados_controller.rb
@@ -1,5 +1,6 @@
 class AliadosController < ApplicationController
   before_action :set_aliado, only: [:show, :edit, :update, :destroy]
+  protect_from_forgery with: :null_session
 
   # GET /aliados
   # GET /aliados.json
